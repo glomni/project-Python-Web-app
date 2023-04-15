4443dff4493696d5fed9d5ef14783ad05dfc604a
glomni <catalinglomnicu1@gmail.com>
2023-04-15 22:22:15 +0300

    add create table in app.py

--- app.py
+++ app.py
@@ -1,27 +1,26 @@
 import os
 import psycopg2
-from flask import Flask, request, jsonify
+from flask import Flask, request
 
+# Create Flask application
 app = Flask(__name__)
 
-# Define a route for handling GET requests to the /messages endpoint
-@app.route('/messages', methods=['GET'])
-def get_messages():
-    conn = psycopg2.connect(
-        dbname=os.environ['POSTGRES_DB'],
-        user=os.environ['POSTGRES_USER'],
-        password=os.environ['POSTGRES_PASSWORD'],
-        host='db'
-    )
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM messages")
-    rows = cur.fetchall()
-    messages = [{'id': row[0], 'text': row[1]} for row in rows]
-    cur.close()
-    conn.close()
-    return {'messages': messages}
+# Connect to PostgreSQL database
+conn = psycopg2.connect(
+    dbname=os.environ['POSTGRES_DB'],
+    user=os.environ['POSTGRES_USER'],
+    password=os.environ['POSTGRES_PASSWORD'],
+    host='db'
+)
 
-# Define a route for handling POST requests to the /messages endpoint
+# Create table for storing messages
+cur = conn.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS messages (id SERIAL PRIMARY KEY, text TEXT)")
+conn.commit()
+cur.close()
+conn.close()
+
+# Create route for receiving POST requests to store messages
 @app.route('/messages', methods=['POST'])
 def create_message():
     message = request.json['message']
@@ -31,12 +30,17 @@ def create_message():
         password=os.environ['POSTGRES_PASSWORD'],
         host='db'
     )
+
+# Insert message into database
     cur = conn.cursor()
     cur.execute("INSERT INTO messages (text) VALUES (%s)", (message,))
     conn.commit()
     cur.close()
     conn.close()
+
+# Return success message
     return 'Message created\n'
 
 if __name__ == '__main__':
+# Run Flask application
     app.run(debug=True, host='0.0.0.0')
